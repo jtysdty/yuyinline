2834e765fea9dfdc28076480d5cb18b1d4d67af5
jtysdty <76022364+jtysdty@users.noreply.github.com>
2021-07-14 16:56:50 +0800

    Update AsrHandler.java

--- server/src/main/java/com/example/server/handler/AsrHandler.java
+++ server/src/main/java/com/example/server/handler/AsrHandler.java
@@ -17,22 +17,10 @@ public class AsrHandler extends Handler{
     @Override
     public void doHandler(RecodeContext recodeContext) throws IOException {
         try{
-//            String appKey = "H44rSxw8EyOL3qo0";
-//            String id = "LTAI5tMmQyQBi2he4QbeHdo1";
-//            String secret = "WmV8XDqVE98GC7Hph3X9uhn7geTROO";
-//            String url = "wss://nls-gateway.cn-shanghai.aliyuncs.com/ws/v1";
-//            // 默认值：wss://nls-gateway.cn-shanghai.aliyuncs.com/ws/v1。
-//            //String filepath = "D:/test/AudioFile/sample.wav";
-//            String filepath = "D:/test/AudioFile/recond3.wav";
-//            AsrUtils demo = new AsrUtils(appKey, id, secret, url);
-//            demo.process(filepath);
-//            String process = demo.shutdown();
-//            System.out.println("asr识别结果："+process);
-//            recodeContext.setAsrResponseText(process);
 
-            String appKey = "H44rSxw8EyOL3qo0";
-            String id = "LTAI5tMmQyQBi2he4QbeHdo1";
-            String secret = "WmV8XDqVE98GC7Hph3X9uhn7geTROO";
+            String appKey = "ali appkey";
+            String id = "自己的id";
+            String secret = "自己的secret";
             String url = "wss://nls-gateway.cn-shanghai.aliyuncs.com/ws/v1";
             // 默认值：wss://nls-gateway.cn-shanghai.aliyuncs.com/ws/v1。
             //String filepath = "D:/test/AudioFile/sample.wav";
